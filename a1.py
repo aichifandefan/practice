3afb6b8d95398127f0fac19e818c7697ec7892cc
aichifandefan <qq292583796@163.com>
2021-01-29 09:20:53 +0800

    this is a test

--- a1.py
+++ a1.py
@@ -1,2 +1,16 @@
 import tensorflow as tf
 tf.__version__
+def main():
+    pass
+
+def insert():
+    pass
+def save():
+    pass
+class System():
+    def __init__(self):
+    pass
+
+#this is a test
+
+
